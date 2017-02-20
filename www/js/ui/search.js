ba8dce386603ed879758295cd62c05c6fc867080
imflyn <flyn.yu@fandine.com>
2017-02-20 17:55:23 +0800

    搜索框

--- www/js/ui/search.js
+++ www/js/ui/search.js
@@ -3,8 +3,23 @@ window.addEventListener('load', function () {
 });
 function onDeviceReady() {
     document.addEventListener('backbutton', onBackKeyDown, false);
+
+    StatusBar.backgroundColorByHexString("#4db6ac");
 }
 function onBackKeyDown() {
     history.go(-1);
     navigator.app.backHistory();
 }
+
+function onSearchFocused() {
+    document.getElementById('img_search_black').style.visibility = "visible";
+    document.getElementById('img_search_white').style.visibility = "hidden";
+}
+
+
+function onSearchBlured() {
+    document.getElementById('img_search_black').style.visibility = "hidden";
+    document.getElementById('img_search_white').style.visibility = "visible";
+}
+
+// document.getElementById("input_search").focus();
