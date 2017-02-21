36e7fb5de336ddcbc7b5ae6fd4175f7529336f59
imflyn <flyn.yu@fandine.com>
2017-02-21 10:41:56 +0800

    搜索框优化

--- www/js/ui/search.js
+++ www/js/ui/search.js
@@ -15,10 +15,12 @@ function onBackKeyDown() {
 function onSearchFocused() {
     document.getElementById('img_search_black').style.visibility = "visible";
     document.getElementById('img_search_white').style.visibility = "hidden";
+    document.getElementById('btn_search_text_clear').style.visibility = 'visible'
 }
 function onSearchBlured() {
     document.getElementById('img_search_black').style.visibility = "hidden";
     document.getElementById('img_search_white').style.visibility = "visible";
+    document.getElementById('btn_search_text_clear').style.visibility = 'hidden'
 }
 function load_history() {
     history_list = localStorage.getItem("search_history");
@@ -28,4 +30,11 @@ function load_history() {
     else {
         history_list = JSON.parse(history_list);
     }
+}
+function search() {
+    var text = document.getElementById('input_search').value;
+
+}
+function search_text_clear() {
+    document.getElementById('btn_search_text_clear').style.visibility = 'hidden'
 }
